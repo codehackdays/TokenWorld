b9ab14545f9c6b7a01ae6d2fe1b4b260f43d12be
codehackdays <codehackdays@users.noreply.github.com>
2017-04-20 00:05:42 +0100

    Try example again

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ app.get('/', function (req, res) {
 });
 
 app.get('/helloworld', function (req, res) {
-  res.send(process.env.CLIENT_ID);
-//  var options = {
-//    method: 'POST',
-//    url: 'https://codehackdays.eu.auth0.com/oauth/token',
-//    headers: { 'content-type': 'application/json' },
-//    body: '{ "audience":"https://codehackdays-helloworld.herokuapp.com/", "client_id":"' + process.env.CLIENT_ID + '", "client_secret":"' + process.env.CLIENT_SECRET + '", "grant_type":"client_credentials" }'
-//  };
+  var options = {
+    method: 'POST',
+    url: 'https://codehackdays.eu.auth0.com/oauth/token',
+    headers: { 'content-type': 'application/json' },
+    body: '{ "audience":"https://codehackdays-helloworld.herokuapp.com/", "client_id":"' + process.env.CLIENT_ID + '", "client_secret":"' + process.env.CLIENT_SECRET + '", "grant_type":"client_credentials" }'
+  };
 
-//  request(options, function (err, response, body) {
-//    if (err) { throw err; }
-//    res.send(body.access_token);
-//  });
+  request(options, function (err, response, body) {
+    if (err) { throw err; }
+    res.send(body.access_token);
+  });
 });
 
 app.listen(process.env.PORT || 3000);
