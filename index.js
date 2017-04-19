6be6f04854c78e0f03091ea5c4571bef7656dfa7
codehackdays <codehackdays@users.noreply.github.com>
2017-04-20 00:10:11 +0100

    Return body

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/helloworld', function (req, res) {
 
   request(options, function (err, response, body) {
     if (err) { throw err; }
-    res.send(body.access_token);
+    res.send(JSON.stringify(body));
   });
 });
 
